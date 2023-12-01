05cab1a7bd90c5e15b0c02c4d184859d6729d7eb
KostPost <kt5082206@gmail.com>
2023-12-02 00:19:08 +0200

    qwe

---
+++ src/main/java/com/kostpost/authorization/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.kostpost.authorization;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
